a53343f5b9123023de8facf04dfce212eb4018aa
Thomas Clark <tnc1997@outlook.com>
2025-10-31 20:34:02 +0000

    refactor: add authority uri constant

--- lib/octopus_energy_api_client.dart
+++ lib/octopus_energy_api_client.dart
@@ -1,1 +1,2 @@
+export 'src/common/constants/uri_constants.dart';
 export 'src/octopus_energy_api_client_base.dart';
---
+++ lib/src/common/constants/uri_constants.dart
@@ -0,0 +1,2 @@
+/// The authority of the Octopus Energy API.
+const authority = 'api.octopus.energy';
