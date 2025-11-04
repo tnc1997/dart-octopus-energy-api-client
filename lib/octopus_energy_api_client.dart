cbab0c93bb11ce69319155c6abc06dac19dba743
Thomas Clark <tnc1997@outlook.com>
2025-11-04 11:11:39 +0000

    refactor: add gas meter point meter model

--- lib/octopus_energy_api_client.dart
+++ lib/octopus_energy_api_client.dart
@@ -1,3 +1,4 @@
+export 'src/accounts/models/gas_meter_point_meter.dart';
 export 'src/accounts/models/meter_point_meter.dart';
 export 'src/common/constants/uri_constants.dart';
 export 'src/common/exceptions/octopus_energy_api_client_exception.dart';
