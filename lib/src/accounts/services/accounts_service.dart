af9c0017f9e5d6e3d45d661e900f10c3b79a5753
Thomas Clark <tnc1997@outlook.com>
2025-11-17 09:20:14 +0000

    feat: add get account endpoint

--- lib/src/accounts/services/accounts_service.dart
+++ lib/src/accounts/services/accounts_service.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+
+import '../../common/constants/uri_constants.dart';
+import '../../common/exceptions/octopus_energy_api_client_exception.dart';
+import '../models/account.dart';
 
 class AccountsService {
   final http.Client _client;
@@ -6,4 +12,20 @@ class AccountsService {
   const AccountsService({
     required http.Client client,
   }) : _client = client;
+
+  /// Retrieve the details of an account.
+  Future<Account> getAccount(
+    String accountNumber,
+  ) async {
+    final response = await _client.get(
+      Uri.https(
+        authority,
+        '/v1/accounts/$accountNumber/',
+      ),
+    );
+
+    OctopusEnergyApiClientException.checkIsSuccessStatusCode(response);
+
+    return Account.fromJson(json.decode(response.body));
+  }
 }
