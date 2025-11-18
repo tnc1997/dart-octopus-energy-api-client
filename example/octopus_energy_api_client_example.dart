4540f234191eb84710e1015a4a8e40cd4511ea19
Thomas Clark <tnc1997@outlook.com>
2025-11-18 08:09:39 +0000

    refactor: add octopus energy api client example

--- example/octopus_energy_api_client_example.dart
+++ example/octopus_energy_api_client_example.dart
@@ -1,3 +1,19 @@
+import 'dart:io';
+
 import 'package:octopus_energy_api_client/octopus_energy_api_client.dart';
 
-void main() {}
+Future<void> main() async {
+  final client = clientViaApiKey(
+    Platform.environment['API_KEY']!,
+  );
+
+  final api = OctopusEnergyApiClient(
+    client: client,
+  );
+
+  final result = await api.accounts.getAccount(
+    Platform.environment['ACCOUNT_NUMBER']!,
+  );
+
+  client.close();
+}
