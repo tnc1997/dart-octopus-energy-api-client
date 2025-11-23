c78963f2f50c34cde7aff60f5316e1c0d10a8d67
Thomas Clark <tnc1997@outlook.com>
2025-11-23 09:28:51 +0000

    refactor: add payment method enum

---
+++ lib/src/accounts/enums/payment_method.dart
@@ -0,0 +1,31 @@
+enum PaymentMethod {
+  billTriggeredDirectDebit,
+  monthlyDirectDebit,
+  monthlyDirectPayment;
+
+  factory PaymentMethod.fromJson(
+    String json,
+  ) {
+    switch (json) {
+      case 'BILL_TRIGGERED_DIRECT_DEBIT':
+        return PaymentMethod.billTriggeredDirectDebit;
+      case 'MONTHLY_DIRECT_DEBIT':
+        return PaymentMethod.monthlyDirectDebit;
+      case 'MONTHLY_DIRECT_PAYMENT':
+        return PaymentMethod.monthlyDirectPayment;
+      default:
+        throw ArgumentError.value(json, 'json');
+    }
+  }
+
+  String toJson() {
+    switch (this) {
+      case PaymentMethod.billTriggeredDirectDebit:
+        return 'BILL_TRIGGERED_DIRECT_DEBIT';
+      case PaymentMethod.monthlyDirectDebit:
+        return 'MONTHLY_DIRECT_DEBIT';
+      case PaymentMethod.monthlyDirectPayment:
+        return 'MONTHLY_DIRECT_PAYMENT';
+    }
+  }
+}
