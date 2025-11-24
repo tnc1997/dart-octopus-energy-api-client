464d412d3e394edcab4689b9c29e64140cf45a22
Thomas Clark <tnc1997@outlook.com>
2025-11-24 16:15:49 +0000

    test: add account source enum

---
+++ test/enums/account_source_test.dart
@@ -0,0 +1,60 @@
+import 'package:octopus_energy_api_client/octopus_energy_api_client.dart';
+import 'package:test/test.dart';
+
+const _cases = {
+  AccountSource.callCentreInbound: 'CALL_CENTRE_INBOUND',
+  AccountSource.callCentreOutbound: 'CALL_CENTRE_OUTBOUND',
+  AccountSource.faceToFace: 'FACE_TO_FACE',
+  AccountSource.internet: 'INTERNET',
+};
+
+void main() {
+  group(
+    'AccountSource',
+    () {
+      group(
+        'fromJson',
+        () {
+          test(
+            'should map the json string to an enum value',
+            () {
+              for (final entry in _cases.entries) {
+                expect(
+                  AccountSource.fromJson(entry.value),
+                  entry.key,
+                );
+              }
+            },
+          );
+
+          test(
+            'should throw an argument error when the json string is invalid',
+            () {
+              expect(
+                () => AccountSource.fromJson(''),
+                throwsA(isA<ArgumentError>()),
+              );
+            },
+          );
+        },
+      );
+
+      group(
+        'toJson',
+        () {
+          test(
+            'should map the enum value to a json string',
+            () {
+              for (final entry in _cases.entries) {
+                expect(
+                  entry.key.toJson(),
+                  entry.value,
+                );
+              }
+            },
+          );
+        },
+      );
+    },
+  );
+}
