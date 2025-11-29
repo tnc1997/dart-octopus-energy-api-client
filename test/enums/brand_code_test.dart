95cd08a69c581e8bee0ec8b81ec7a1a54492558e
Thomas Clark <tnc1997@outlook.com>
2025-11-29 21:21:39 +0000

    test: add brand code enum

---
+++ test/enums/brand_code_test.dart
@@ -0,0 +1,59 @@
+import 'package:octopus_energy_api_client/octopus_energy_api_client.dart';
+import 'package:test/test.dart';
+
+const _cases = {
+  BrandCode.coopEnergy: 'COOP_ENERGY',
+  BrandCode.londonPower: 'LONDON_POWER',
+  BrandCode.octopusEnergy: 'OCTOPUS_ENERGY',
+};
+
+void main() {
+  group(
+    'BrandCode',
+    () {
+      group(
+        'fromJson',
+        () {
+          test(
+            'should map the json string to an enum value',
+            () {
+              for (final entry in _cases.entries) {
+                expect(
+                  BrandCode.fromJson(entry.value),
+                  entry.key,
+                );
+              }
+            },
+          );
+
+          test(
+            'should throw an argument error when the json string is invalid',
+            () {
+              expect(
+                () => BrandCode.fromJson(''),
+                throwsA(isA<ArgumentError>()),
+              );
+            },
+          );
+        },
+      );
+
+      group(
+        'toJson',
+        () {
+          test(
+            'should map the enum value to a json string',
+            () {
+              for (final entry in _cases.entries) {
+                expect(
+                  entry.key.toJson(),
+                  entry.value,
+                );
+              }
+            },
+          );
+        },
+      );
+    },
+  );
+}
