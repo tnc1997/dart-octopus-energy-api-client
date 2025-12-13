c0b0360f65a4ba8e93bc73115c6d16191b90d965
Thomas Clark <tnc1997@outlook.com>
2025-12-13 16:04:05 +0000

    refactor: add user model

--- lib/octopus_energy_api_client.dart
+++ lib/octopus_energy_api_client.dart
@@ -31,6 +31,7 @@ export 'src/accounts/models/property.dart';
 export 'src/accounts/models/psr.dart';
 export 'src/accounts/models/quote.dart';
 export 'src/accounts/models/register.dart';
+export 'src/accounts/models/user.dart';
 export 'src/accounts/services/accounts_service.dart';
 export 'src/common/clients/api_key_client.dart';
 export 'src/common/constants/uri_constants.dart';
---
+++ lib/src/accounts/models/user.dart
@@ -0,0 +1,75 @@
+import 'credit_score_data.dart';
+import 'psr.dart';
+
+class User {
+  CreditScoreData? creditScoreData;
+  DateTime? dateOfBirth;
+  String email;
+  String familyName;
+  String givenName;
+  String? label;
+  String? landline;
+  String? mobile;
+  bool optedInForMarketing;
+  bool? optedInToThirdParties;
+  Psr? psr;
+  String? title;
+
+  User({
+    this.creditScoreData,
+    this.dateOfBirth,
+    required this.email,
+    required this.familyName,
+    required this.givenName,
+    this.label,
+    this.landline,
+    this.mobile,
+    required this.optedInForMarketing,
+    this.optedInToThirdParties,
+    this.psr,
+    this.title,
+  });
+
+  factory User.fromJson(
+    Map<String, dynamic> json,
+  ) {
+    return User(
+      creditScoreData: json['credit_score_data'] != null
+          ? CreditScoreData.fromJson(
+              json['credit_score_data'] as Map<String, dynamic>)
+          : null,
+      dateOfBirth: json['date_of_birth'] != null
+          ? DateTime.parse(json['date_of_birth'] as String)
+          : null,
+      email: json['email'] as String,
+      familyName: json['family_name'] as String,
+      givenName: json['given_name'] as String,
+      label: json['label'] as String?,
+      landline: json['landline'] as String?,
+      mobile: json['mobile'] as String?,
+      optedInForMarketing: json['opted_in_for_marketing'] as bool,
+      optedInToThirdParties: json['opted_in_to_third_parties'] as bool?,
+      psr: json['psr'] != null
+          ? Psr.fromJson(json['psr'] as Map<String, dynamic>)
+          : null,
+      title: json['title'] as String?,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'credit_score_data': creditScoreData?.toJson(),
+      'date_of_birth': dateOfBirth?.toIso8601String(),
+      'email': email,
+      'family_name': familyName,
+      'given_name': givenName,
+      'label': label,
+      'landline': landline,
+      'mobile': mobile,
+      'opted_in_for_marketing': optedInForMarketing,
+      'opted_in_to_third_parties': optedInToThirdParties,
+      'psr': psr?.toJson(),
+      'title': title,
+    };
+  }
+}
