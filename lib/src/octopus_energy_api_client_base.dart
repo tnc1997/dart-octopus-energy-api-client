013896b2af320199f859a1afb9ca6c8783f0954d
Thomas Clark <tnc1997@outlook.com>
2026-01-12 17:11:11 +0000

    refactor: add accounts service to client

--- lib/src/octopus_energy_api_client_base.dart
+++ lib/src/octopus_energy_api_client_base.dart
@@ -1,9 +1,19 @@
 import 'package:http/http.dart' as http;
 
+import 'accounts/services/accounts_service.dart';
+
 class OctopusEnergyApiClient {
   final http.Client _client;
+
+  AccountsService? _accounts;
 
   OctopusEnergyApiClient({
     http.Client? client,
   }) : _client = client ?? http.Client();
+
+  AccountsService get accounts {
+    return _accounts ??= AccountsService(
+      client: _client,
+    );
+  }
 }
