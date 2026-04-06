a9dd5fe7c049ca7d9e6456e7e2d398473d856a0f
Thomas Clark <tnc1997@outlook.com>
2026-04-06 07:34:07 +0100

    refactor(electricity-meter-points): add service to client

--- lib/src/octopus_energy_api_client_base.dart
+++ lib/src/octopus_energy_api_client_base.dart
@@ -1,11 +1,13 @@
 import 'package:http/http.dart' as http;
 
 import 'accounts/services/accounts_service.dart';
+import 'electricity_meter_points/services/electricity_meter_points_service.dart';
 
 class OctopusEnergyApiClient {
   final http.Client _client;
 
   AccountsService? _accounts;
+  ElectricityMeterPointsService? _electricityMeterPoints;
 
   OctopusEnergyApiClient({
     http.Client? client,
@@ -13,6 +15,12 @@ class OctopusEnergyApiClient {
 
   AccountsService get accounts {
     return _accounts ??= AccountsService(
+      client: _client,
+    );
+  }
+
+  ElectricityMeterPointsService get electricityMeterPoints {
+    return _electricityMeterPoints ??= ElectricityMeterPointsService(
       client: _client,
     );
   }
