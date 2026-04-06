46dc30811787a9eacb1b184cbe759af219fa51a7
Thomas Clark <tnc1997@outlook.com>
2026-04-06 07:31:56 +0100

    feat(electricity-meter-points): add get electricity meter point endpoint

--- lib/src/electricity_meter_points/services/electricity_meter_points_service.dart
+++ lib/src/electricity_meter_points/services/electricity_meter_points_service.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+
+import '../../common/constants/uri_constants.dart';
+import '../../common/exceptions/octopus_energy_api_client_exception.dart';
+import '../models/electricity_meter_point.dart';
 
 class ElectricityMeterPointsService {
   final http.Client _client;
@@ -6,4 +12,20 @@ class ElectricityMeterPointsService {
   const ElectricityMeterPointsService({
     required http.Client client,
   }) : _client = client;
+
+  /// Retrieve the details of a meter-point. This endpoint can be used to get the GSP of a given meter-point.
+  Future<ElectricityMeterPoint> getElectricityMeterPoint(
+    String mpan,
+  ) async {
+    final response = await _client.get(
+      Uri.https(
+        authority,
+        '/v1/electricity-meter-points/$mpan/',
+      ),
+    );
+
+    OctopusEnergyApiClientException.checkIsSuccessStatusCode(response);
+
+    return ElectricityMeterPoint.fromJson(json.decode(response.body));
+  }
 }
