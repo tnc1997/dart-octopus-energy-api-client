03af0608c2d6e172cedfaac459ec71df8549365f
Thomas Clark <tnc1997@outlook.com>
2026-04-07 17:07:03 +0100

    refactor(gas-meter-points): add service

--- lib/octopus_energy_api_client.dart
+++ lib/octopus_energy_api_client.dart
@@ -58,4 +58,5 @@ export 'src/common/models/quote.dart';
 export 'src/electricity_meter_points/models/electricity_meter_point.dart';
 export 'src/electricity_meter_points/services/electricity_meter_points_service.dart';
 export 'src/gas_meter_points/models/gas_meter_point.dart';
+export 'src/gas_meter_points/services/gas_meter_points_service.dart';
 export 'src/octopus_energy_api_client_base.dart';
