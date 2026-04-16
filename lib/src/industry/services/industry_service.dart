8fdc24eab4d4148c88194a2f41e5e4a906821397
Thomas Clark <tnc1997@outlook.com>
2026-04-16 21:11:33 +0100

    feat(industry): add list industry grid supply points endpoint

--- lib/src/industry/services/industry_service.dart
+++ lib/src/industry/services/industry_service.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+
+import '../../common/constants/uri_constants.dart';
+import '../../common/exceptions/octopus_energy_api_client_exception.dart';
+import '../models/paginated_grid_supply_point_list.dart';
 
 class IndustryService {
   final http.Client _client;
@@ -6,4 +12,25 @@ class IndustryService {
   const IndustryService({
     required http.Client client,
   }) : _client = client;
+
+  /// Return a list of GSP objects, optionally filtered by postcode. If we’re unable to map the passed postcode to a GSP, an empty list will be returned.
+  Future<PaginatedGridSupplyPointList> listIndustryGridSupplyPoints({
+    int? page,
+    String? postcode,
+  }) async {
+    final response = await _client.get(
+      Uri.https(
+        authority,
+        '/v1/industry/grid-supply-points/',
+        {
+          if (page != null) 'page': page.toString(),
+          if (postcode != null) 'postcode': postcode,
+        },
+      ),
+    );
+
+    OctopusEnergyApiClientException.checkIsSuccessStatusCode(response);
+
+    return PaginatedGridSupplyPointList.fromJson(json.decode(response.body));
+  }
 }
