44aa4c52e7d339dcaee708053fe5b275c64898c6
Thomas Clark <tnc1997@outlook.com>
2026-04-29 13:31:04 +0100

    refactor(products): add standard electricity tariff model

---
+++ lib/src/products/models/standard_electricity_tariff.dart
@@ -0,0 +1,77 @@
+import '../../common/models/link.dart';
+
+class StandardElectricityTariff {
+  String code;
+  double dualFuelDiscountExcVat;
+  double dualFuelDiscountIncVat;
+  double exitFeesExcVat;
+  double exitFeesIncVat;
+  String exitFeesType;
+  List<Link> links;
+  double onlineDiscountExcVat;
+  double onlineDiscountIncVat;
+  double standingChargeExcVat;
+  double standingChargeIncVat;
+  double standardUnitRateExcVat;
+  double standardUnitRateIncVat;
+
+  StandardElectricityTariff({
+    required this.code,
+    required this.dualFuelDiscountExcVat,
+    required this.dualFuelDiscountIncVat,
+    required this.exitFeesExcVat,
+    required this.exitFeesIncVat,
+    required this.exitFeesType,
+    required this.links,
+    required this.onlineDiscountExcVat,
+    required this.onlineDiscountIncVat,
+    required this.standingChargeExcVat,
+    required this.standingChargeIncVat,
+    required this.standardUnitRateExcVat,
+    required this.standardUnitRateIncVat,
+  });
+
+  factory StandardElectricityTariff.fromJson(
+    Map<String, dynamic> json,
+  ) {
+    return StandardElectricityTariff(
+      code: json['code'] as String,
+      dualFuelDiscountExcVat:
+          (json['dual_fuel_discount_exc_vat'] as num).toDouble(),
+      dualFuelDiscountIncVat:
+          (json['dual_fuel_discount_inc_vat'] as num).toDouble(),
+      exitFeesExcVat: (json['exit_fees_exc_vat'] as num).toDouble(),
+      exitFeesIncVat: (json['exit_fees_inc_vat'] as num).toDouble(),
+      exitFeesType: json['exit_fees_type'] as String,
+      links: (json['links'] as List<dynamic>)
+          .map((e) => Link.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      onlineDiscountExcVat: (json['online_discount_exc_vat'] as num).toDouble(),
+      onlineDiscountIncVat: (json['online_discount_inc_vat'] as num).toDouble(),
+      standingChargeExcVat: (json['standing_charge_exc_vat'] as num).toDouble(),
+      standingChargeIncVat: (json['standing_charge_inc_vat'] as num).toDouble(),
+      standardUnitRateExcVat:
+          (json['standard_unit_rate_exc_vat'] as num).toDouble(),
+      standardUnitRateIncVat:
+          (json['standard_unit_rate_inc_vat'] as num).toDouble(),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'code': code,
+      'dual_fuel_discount_exc_vat': dualFuelDiscountExcVat,
+      'dual_fuel_discount_inc_vat': dualFuelDiscountIncVat,
+      'exit_fees_exc_vat': exitFeesExcVat,
+      'exit_fees_inc_vat': exitFeesIncVat,
+      'exit_fees_type': exitFeesType,
+      'links': links.map((e) => e.toJson()).toList(),
+      'online_discount_exc_vat': onlineDiscountExcVat,
+      'online_discount_inc_vat': onlineDiscountIncVat,
+      'standing_charge_exc_vat': standingChargeExcVat,
+      'standing_charge_inc_vat': standingChargeIncVat,
+      'standard_unit_rate_exc_vat': standardUnitRateExcVat,
+      'standard_unit_rate_inc_vat': standardUnitRateIncVat,
+    };
+  }
+}
