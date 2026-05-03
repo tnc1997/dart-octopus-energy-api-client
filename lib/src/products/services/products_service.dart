5aae6b4765adbd42e7a97b0ce45d7b8bc12b74f8
Thomas Clark <tnc1997@outlook.com>
2026-05-03 08:49:33 +0100

    feat(products): add list electricity tariff standard unit rates endpoint

--- lib/src/products/services/products_service.dart
+++ lib/src/products/services/products_service.dart
@@ -49,6 +49,23 @@ class ProductsService {
     );
   }
 
+  Future<PaginatedHistoricalChargeList> listElectricityTariffStandardUnitRates(
+    String productCode,
+    String tariffCode, {
+    int? page,
+    int? pageSize,
+    DateTime? periodFrom,
+    DateTime? periodTo,
+  }) {
+    return _listTariffUnitRates(
+      '/v1/products/$productCode/electricity-tariffs/$tariffCode/standard-unit-rates/',
+      page: page,
+      pageSize: pageSize,
+      periodFrom: periodFrom,
+      periodTo: periodTo,
+    );
+  }
+
   /// Return a list of energy products. By default, results will be public energy products but if the authenticated organisations will also see products available to their organisation.
   Future<PaginatedProductsList> listProducts({
     DateTime? availableAt,
