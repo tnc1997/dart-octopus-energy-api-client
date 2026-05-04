805493ce355fdc8d6efacc428d74ee930abac4e7
Thomas Clark <tnc1997@outlook.com>
2026-05-04 16:54:08 +0100

    refactor(ivr-support): add service to client

--- lib/src/octopus_energy_api_client_base.dart
+++ lib/src/octopus_energy_api_client_base.dart
@@ -4,6 +4,7 @@ import 'accounts/services/accounts_service.dart';
 import 'electricity_meter_points/services/electricity_meter_points_service.dart';
 import 'gas_meter_points/services/gas_meter_points_service.dart';
 import 'industry/services/industry_service.dart';
+import 'ivr_support/services/ivr_support_service.dart';
 
 class OctopusEnergyApiClient {
   final http.Client _client;
@@ -12,6 +13,7 @@ class OctopusEnergyApiClient {
   ElectricityMeterPointsService? _electricityMeterPoints;
   GasMeterPointsService? _gasMeterPoints;
   IndustryService? _industry;
+  IvrSupportService? _ivrSupport;
 
   OctopusEnergyApiClient({
     http.Client? client,
@@ -37,6 +39,12 @@ class OctopusEnergyApiClient {
 
   IndustryService get industry {
     return _industry ??= IndustryService(
+      client: _client,
+    );
+  }
+
+  IvrSupportService get ivrSupport {
+    return _ivrSupport ??= IvrSupportService(
       client: _client,
     );
   }
