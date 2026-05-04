1ee8992248369110eb167cb8f54b1d31cf7999f4
Thomas Clark <tnc1997@outlook.com>
2026-05-04 10:39:10 +0100

    test(products): add direction enum

---
+++ test/enums/direction_test.dart
@@ -0,0 +1,58 @@
+import 'package:octopus_energy_api_client/octopus_energy_api_client.dart';
+import 'package:test/test.dart';
+
+const _cases = {
+  Direction.export: 'EXPORT',
+  Direction.import: 'IMPORT',
+};
+
+void main() {
+  group(
+    'Direction',
+    () {
+      group(
+        'fromJson',
+        () {
+          test(
+            'should map the json string to an enum value',
+            () {
+              for (final entry in _cases.entries) {
+                expect(
+                  Direction.fromJson(entry.value),
+                  entry.key,
+                );
+              }
+            },
+          );
+
+          test(
+            'should throw an argument error when the json string is invalid',
+            () {
+              expect(
+                () => Direction.fromJson(''),
+                throwsA(isA<ArgumentError>()),
+              );
+            },
+          );
+        },
+      );
+
+      group(
+        'toJson',
+        () {
+          test(
+            'should map the enum value to a json string',
+            () {
+              for (final entry in _cases.entries) {
+                expect(
+                  entry.key.toJson(),
+                  entry.value,
+                );
+              }
+            },
+          );
+        },
+      );
+    },
+  );
+}
