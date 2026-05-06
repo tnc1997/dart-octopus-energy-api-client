671f390c13f569c2b6d943f5d47de0eeb6d21ea7
Thomas Clark <tnc1997@outlook.com>
2026-05-06 17:49:08 +0100

    test(common): add api key client

---
+++ test/common/clients/api_key_client_test.dart
@@ -0,0 +1,128 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:http/testing.dart';
+import 'package:octopus_energy_api_client/octopus_energy_api_client.dart';
+import 'package:test/test.dart';
+
+void main() {
+  const apiKey = 'sk_test_abc';
+
+  group(
+    'ApiKeyClient',
+    () {
+      group(
+        'close',
+        () {
+          test('closes inner client', () {
+            var closed = false;
+
+            ApiKeyClient(
+              apiKey: apiKey,
+              inner: MockCloseClient(
+                () {
+                  closed = true;
+                },
+              ),
+            ).close();
+
+            expect(
+              closed,
+              isTrue,
+            );
+          });
+        },
+      );
+
+      group(
+        'send',
+        () {
+          test(
+            'adds authorization header',
+            () async {
+              await ApiKeyClient(
+                apiKey: apiKey,
+                inner: MockClient(
+                  (request) async {
+                    expect(
+                      request.headers['authorization'],
+                      'Basic ${base64.encode(utf8.encode('$apiKey:'))}',
+                    );
+
+                    return http.Response('', 200);
+                  },
+                ),
+              ).get(
+                Uri.https(
+                  'example.com',
+                ),
+              );
+            },
+          );
+
+          test(
+            'throws ArgumentError if authorization header is already present',
+            () async {
+              expect(
+                () => ApiKeyClient(
+                  apiKey: apiKey,
+                  inner: MockClient(
+                    (request) async {
+                      return http.Response('', 200);
+                    },
+                  ),
+                ).get(
+                  Uri.https(
+                    'example.com',
+                  ),
+                  headers: {
+                    'authorization': 'hello world',
+                  },
+                ),
+                throwsA(isA<ArgumentError>()),
+              );
+            },
+          );
+        },
+      );
+    },
+  );
+
+  group(
+    'clientViaApiKey',
+    () {
+      test(
+        'returns ApiKeyClient',
+        () {
+          expect(
+            clientViaApiKey(apiKey),
+            isA<ApiKeyClient>(),
+          );
+        },
+      );
+    },
+  );
+}
+
+class MockCloseClient extends http.BaseClient {
+  final void Function() onClose;
+
+  MockCloseClient(this.onClose);
+
+  @override
+  void close() {
+    onClose();
+
+    super.close();
+  }
+
+  @override
+  Future<http.StreamedResponse> send(
+    http.BaseRequest request,
+  ) async {
+    return http.StreamedResponse(
+      const Stream.empty(),
+      200,
+    );
+  }
+}
