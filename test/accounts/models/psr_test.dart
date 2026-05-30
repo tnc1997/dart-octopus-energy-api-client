7047320027caeffa64a046cb85d05499b76ba25e
Thomas Clark <tnc1997@outlook.com>
2026-05-30 06:33:09 +0100

    test(accounts): add psr model

---
+++ test/accounts/models/psr_test.dart
@@ -0,0 +1,79 @@
+import 'package:octopus_energy_api_client/octopus_energy_api_client.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group(
+    'Psr',
+    () {
+      group(
+        'fromJson',
+        () {
+          test(
+            'should return model from json',
+            () {
+              final json = {
+                'requires_signup': true,
+              };
+
+              final result = Psr.fromJson(json);
+
+              expect(
+                result.requiresSignup,
+                isTrue,
+              );
+            },
+          );
+
+          test(
+            'should return model from json with null values',
+            () {
+              final json = <String, dynamic>{};
+
+              final result = Psr.fromJson(json);
+
+              expect(
+                result.requiresSignup,
+                isNull,
+              );
+            },
+          );
+        },
+      );
+
+      group(
+        'toJson',
+        () {
+          test(
+            'should serialize model to json',
+            () {
+              final model = Psr(
+                requiresSignup: true,
+              );
+
+              final result = model.toJson();
+
+              expect(
+                result['requires_signup'],
+                isTrue,
+              );
+            },
+          );
+
+          test(
+            'should serialize model with null values to json',
+            () {
+              final model = Psr();
+
+              final result = model.toJson();
+
+              expect(
+                result['requires_signup'],
+                isNull,
+              );
+            },
+          );
+        },
+      );
+    },
+  );
+}
