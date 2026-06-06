f1831d54d0e6cbeef6a2b06a0caed628965f61ee
Thomas Clark <tnc1997@outlook.com>
2026-06-06 14:00:59 +0100

    test(products): add paginated products list model

---
+++ test/products/models/paginated_products_list_test.dart
@@ -0,0 +1,257 @@
+import 'package:octopus_energy_api_client/octopus_energy_api_client.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group(
+    'PaginatedProductsList',
+    () {
+      group(
+        'fromJson',
+        () {
+          test(
+            'should return model from json',
+            () {
+              final json = {
+                'count': 123,
+                'next': 'https://api.octopus.energy/v1/products/?page=4',
+                'previous': 'https://api.octopus.energy/v1/products/?page=2',
+                'results': [
+                  {
+                    'available_from': '2017-05-05T05:37:27Z',
+                    'available_to': null,
+                    'brand': 'TENTACLE_ENERGY',
+                    'code': 'VAR-17-01-11',
+                    'description':
+                        'This great value 12 month fixed tariff guarantees…',
+                    'direction': 'IMPORT',
+                    'display_name': 'Flexible Tentacle',
+                    'full_name': 'Flexible Tentacle Offer January 2017 v1',
+                    'is_business': false,
+                    'is_green': false,
+                    'is_prepay': false,
+                    'is_restricted': false,
+                    'is_tracker': false,
+                    'is_variable': true,
+                    'links': [
+                      {
+                        'href':
+                            'https://api.octopus.energy/v1/products/VAR-17-01-11/',
+                        'method': 'GET',
+                        'rel': 'self',
+                      }
+                    ],
+                    'term': 12,
+                  }
+                ],
+              };
+
+              final result = PaginatedProductsList.fromJson(json);
+
+              expect(
+                result.count,
+                123,
+              );
+
+              expect(
+                result.next,
+                'https://api.octopus.energy/v1/products/?page=4',
+              );
+
+              expect(
+                result.previous,
+                'https://api.octopus.energy/v1/products/?page=2',
+              );
+
+              expect(
+                result.results,
+                hasLength(1),
+              );
+            },
+          );
+
+          test(
+            'should return model from json with null values',
+            () {
+              final json = {
+                'count': 123,
+                'results': [
+                  {
+                    'available_from': '2017-05-05T05:37:27Z',
+                    'available_to': null,
+                    'brand': 'TENTACLE_ENERGY',
+                    'code': 'VAR-17-01-11',
+                    'description':
+                        'This great value 12 month fixed tariff guarantees…',
+                    'direction': 'IMPORT',
+                    'display_name': 'Flexible Tentacle',
+                    'full_name': 'Flexible Tentacle Offer January 2017 v1',
+                    'is_business': false,
+                    'is_green': false,
+                    'is_prepay': false,
+                    'is_restricted': false,
+                    'is_tracker': false,
+                    'is_variable': true,
+                    'links': [
+                      {
+                        'href':
+                            'https://api.octopus.energy/v1/products/VAR-17-01-11/',
+                        'method': 'GET',
+                        'rel': 'self',
+                      }
+                    ],
+                    'term': 12,
+                  }
+                ],
+              };
+
+              final result = PaginatedProductsList.fromJson(json);
+
+              expect(
+                result.count,
+                123,
+              );
+
+              expect(
+                result.next,
+                isNull,
+              );
+
+              expect(
+                result.previous,
+                isNull,
+              );
+
+              expect(
+                result.results,
+                hasLength(1),
+              );
+            },
+          );
+        },
+      );
+
+      group(
+        'toJson',
+        () {
+          test(
+            'should serialize model to json',
+            () {
+              final model = PaginatedProductsList(
+                count: 123,
+                next: 'https://api.octopus.energy/v1/products/?page=4',
+                previous: 'https://api.octopus.energy/v1/products/?page=2',
+                results: [
+                  Products(
+                    availableFrom: DateTime.parse('2017-05-05T05:37:27Z'),
+                    brand: 'TENTACLE_ENERGY',
+                    code: 'VAR-17-01-11',
+                    description:
+                        'This great value 12 month fixed tariff guarantees…',
+                    direction: Direction.import,
+                    displayName: 'Flexible Tentacle',
+                    fullName: 'Flexible Tentacle Offer January 2017 v1',
+                    isBusiness: false,
+                    isGreen: false,
+                    isPrepay: false,
+                    isRestricted: false,
+                    isTracker: false,
+                    isVariable: true,
+                    links: [
+                      Link(
+                        href:
+                            'https://api.octopus.energy/v1/products/VAR-17-01-11/',
+                        method: 'GET',
+                        rel: 'self',
+                      ),
+                    ],
+                    term: 12,
+                  ),
+                ],
+              );
+
+              final result = model.toJson();
+
+              expect(
+                result['count'],
+                123,
+              );
+
+              expect(
+                result['next'],
+                'https://api.octopus.energy/v1/products/?page=4',
+              );
+
+              expect(
+                result['previous'],
+                'https://api.octopus.energy/v1/products/?page=2',
+              );
+
+              expect(
+                result['results'],
+                hasLength(1),
+              );
+            },
+          );
+
+          test(
+            'should serialize model with null values to json',
+            () {
+              final model = PaginatedProductsList(
+                count: 123,
+                results: [
+                  Products(
+                    availableFrom: DateTime.parse('2017-05-05T05:37:27Z'),
+                    brand: 'TENTACLE_ENERGY',
+                    code: 'VAR-17-01-11',
+                    description:
+                        'This great value 12 month fixed tariff guarantees…',
+                    direction: Direction.import,
+                    displayName: 'Flexible Tentacle',
+                    fullName: 'Flexible Tentacle Offer January 2017 v1',
+                    isBusiness: false,
+                    isGreen: false,
+                    isPrepay: false,
+                    isRestricted: false,
+                    isTracker: false,
+                    isVariable: true,
+                    links: [
+                      Link(
+                        href:
+                            'https://api.octopus.energy/v1/products/VAR-17-01-11/',
+                        method: 'GET',
+                        rel: 'self',
+                      ),
+                    ],
+                    term: 12,
+                  ),
+                ],
+              );
+
+              final result = model.toJson();
+
+              expect(
+                result['count'],
+                123,
+              );
+
+              expect(
+                result['next'],
+                isNull,
+              );
+
+              expect(
+                result['previous'],
+                isNull,
+              );
+
+              expect(
+                result['results'],
+                hasLength(1),
+              );
+            },
+          );
+        },
+      );
+    },
+  );
+}
