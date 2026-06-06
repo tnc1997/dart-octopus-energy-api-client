79e72346334a8e6524b1f7684bcfbd118164aa97
Thomas Clark <tnc1997@outlook.com>
2026-06-06 14:05:57 +0100

    test(products): add products model

---
+++ test/products/models/products_test.dart
@@ -0,0 +1,458 @@
+import 'package:octopus_energy_api_client/octopus_energy_api_client.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group(
+    'Products',
+    () {
+      group(
+        'fromJson',
+        () {
+          test(
+            'should return model from json',
+            () {
+              final json = {
+                'available_from': '2017-05-05T05:37:27Z',
+                'available_to': null,
+                'brand': 'TENTACLE_ENERGY',
+                'code': 'VAR-17-01-11',
+                'description':
+                    'This great value 12 month fixed tariff guarantees…',
+                'direction': 'IMPORT',
+                'display_name': 'Flexible Tentacle',
+                'full_name': 'Flexible Tentacle Offer January 2017 v1',
+                'is_business': false,
+                'is_green': false,
+                'is_prepay': false,
+                'is_restricted': false,
+                'is_tracker': false,
+                'is_variable': true,
+                'links': [
+                  {
+                    'href':
+                        'https://api.octopus.energy/v1/products/VAR-17-01-11/',
+                    'method': 'GET',
+                    'rel': 'self',
+                  }
+                ],
+                'term': 12,
+              };
+
+              final result = Products.fromJson(json);
+
+              expect(
+                result.availableFrom,
+                DateTime.parse('2017-05-05T05:37:27Z'),
+              );
+
+              expect(
+                result.availableTo,
+                isNull,
+              );
+
+              expect(
+                result.brand,
+                'TENTACLE_ENERGY',
+              );
+
+              expect(
+                result.code,
+                'VAR-17-01-11',
+              );
+
+              expect(
+                result.description,
+                'This great value 12 month fixed tariff guarantees…',
+              );
+
+              expect(
+                result.direction,
+                Direction.import,
+              );
+
+              expect(
+                result.displayName,
+                'Flexible Tentacle',
+              );
+
+              expect(
+                result.fullName,
+                'Flexible Tentacle Offer January 2017 v1',
+              );
+
+              expect(
+                result.isBusiness,
+                false,
+              );
+
+              expect(
+                result.isGreen,
+                false,
+              );
+
+              expect(
+                result.isPrepay,
+                false,
+              );
+
+              expect(
+                result.isRestricted,
+                false,
+              );
+
+              expect(
+                result.isTracker,
+                false,
+              );
+
+              expect(
+                result.isVariable,
+                true,
+              );
+
+              expect(
+                result.links,
+                hasLength(1),
+              );
+
+              expect(
+                result.term,
+                12,
+              );
+            },
+          );
+
+          test(
+            'should return model from json with null values',
+            () {
+              final json = {
+                'available_from': '2017-05-05T05:37:27Z',
+                'brand': 'TENTACLE_ENERGY',
+                'code': 'VAR-17-01-11',
+                'description':
+                    'This great value 12 month fixed tariff guarantees…',
+                'display_name': 'Flexible Tentacle',
+                'full_name': 'Flexible Tentacle Offer January 2017 v1',
+                'is_restricted': false,
+                'links': [
+                  {
+                    'href':
+                    'https://api.octopus.energy/v1/products/VAR-17-01-11/',
+                    'method': 'GET',
+                    'rel': 'self',
+                  }
+                ],
+              };
+
+              final result = Products.fromJson(json);
+
+              expect(
+                result.availableFrom,
+                DateTime.parse('2017-05-05T05:37:27Z'),
+              );
+
+              expect(
+                result.availableTo,
+                isNull,
+              );
+
+              expect(
+                result.brand,
+                'TENTACLE_ENERGY',
+              );
+
+              expect(
+                result.code,
+                'VAR-17-01-11',
+              );
+
+              expect(
+                result.description,
+                'This great value 12 month fixed tariff guarantees…',
+              );
+
+              expect(
+                result.direction,
+                isNull,
+              );
+
+              expect(
+                result.displayName,
+                'Flexible Tentacle',
+              );
+
+              expect(
+                result.fullName,
+                'Flexible Tentacle Offer January 2017 v1',
+              );
+
+              expect(
+                result.isBusiness,
+                isNull,
+              );
+
+              expect(
+                result.isGreen,
+                isNull,
+              );
+
+              expect(
+                result.isPrepay,
+                isNull,
+              );
+
+              expect(
+                result.isRestricted,
+                false,
+              );
+
+              expect(
+                result.isTracker,
+                isNull,
+              );
+
+              expect(
+                result.isVariable,
+                isNull,
+              );
+
+              expect(
+                result.links,
+                hasLength(1),
+              );
+
+              expect(
+                result.term,
+                isNull,
+              );
+            },
+          );
+        },
+      );
+
+      group(
+        'toJson',
+        () {
+          test(
+            'should serialize model to json',
+            () {
+              final model = Products(
+                availableFrom: DateTime.parse('2017-05-05T05:37:27Z'),
+                brand: 'TENTACLE_ENERGY',
+                code: 'VAR-17-01-11',
+                description:
+                    'This great value 12 month fixed tariff guarantees…',
+                direction: Direction.import,
+                displayName: 'Flexible Tentacle',
+                fullName: 'Flexible Tentacle Offer January 2017 v1',
+                isBusiness: false,
+                isGreen: false,
+                isPrepay: false,
+                isRestricted: false,
+                isTracker: false,
+                isVariable: true,
+                links: [
+                  Link(
+                    href:
+                        'https://api.octopus.energy/v1/products/VAR-17-01-11/',
+                    method: 'GET',
+                    rel: 'self',
+                  ),
+                ],
+                term: 12,
+              );
+
+              final result = model.toJson();
+
+              expect(
+                result['available_from'],
+                '2017-05-05T05:37:27.000Z',
+              );
+
+              expect(
+                result['available_to'],
+                isNull,
+              );
+
+              expect(
+                result['brand'],
+                'TENTACLE_ENERGY',
+              );
+
+              expect(
+                result['code'],
+                'VAR-17-01-11',
+              );
+
+              expect(
+                result['description'],
+                'This great value 12 month fixed tariff guarantees…',
+              );
+
+              expect(
+                result['direction'],
+                'IMPORT',
+              );
+
+              expect(
+                result['display_name'],
+                'Flexible Tentacle',
+              );
+
+              expect(
+                result['full_name'],
+                'Flexible Tentacle Offer January 2017 v1',
+              );
+
+              expect(
+                result['is_business'],
+                false,
+              );
+
+              expect(
+                result['is_green'],
+                false,
+              );
+
+              expect(
+                result['is_prepay'],
+                false,
+              );
+
+              expect(
+                result['is_restricted'],
+                false,
+              );
+
+              expect(
+                result['is_tracker'],
+                false,
+              );
+
+              expect(
+                result['is_variable'],
+                true,
+              );
+
+              expect(
+                result['links'],
+                hasLength(1),
+              );
+
+              expect(
+                result['term'],
+                12,
+              );
+            },
+          );
+
+          test(
+            'should serialize model with null values to json',
+            () {
+              final model = Products(
+                availableFrom: DateTime.parse('2017-05-05T05:37:27Z'),
+                brand: 'TENTACLE_ENERGY',
+                code: 'VAR-17-01-11',
+                description:
+                    'This great value 12 month fixed tariff guarantees…',
+                displayName: 'Flexible Tentacle',
+                fullName: 'Flexible Tentacle Offer January 2017 v1',
+                isRestricted: false,
+                links: [
+                  Link(
+                    href:
+                    'https://api.octopus.energy/v1/products/VAR-17-01-11/',
+                    method: 'GET',
+                    rel: 'self',
+                  ),
+                ],
+              );
+
+              final result = model.toJson();
+
+              expect(
+                result['available_from'],
+                '2017-05-05T05:37:27.000Z',
+              );
+
+              expect(
+                result['available_to'],
+                isNull,
+              );
+
+              expect(
+                result['brand'],
+                'TENTACLE_ENERGY',
+              );
+
+              expect(
+                result['code'],
+                'VAR-17-01-11',
+              );
+
+              expect(
+                result['description'],
+                'This great value 12 month fixed tariff guarantees…',
+              );
+
+              expect(
+                result['direction'],
+                isNull,
+              );
+
+              expect(
+                result['display_name'],
+                'Flexible Tentacle',
+              );
+
+              expect(
+                result['full_name'],
+                'Flexible Tentacle Offer January 2017 v1',
+              );
+
+              expect(
+                result['is_business'],
+                isNull,
+              );
+
+              expect(
+                result['is_green'],
+                isNull,
+              );
+
+              expect(
+                result['is_prepay'],
+                isNull,
+              );
+
+              expect(
+                result['is_restricted'],
+                false,
+              );
+
+              expect(
+                result['is_tracker'],
+                isNull,
+              );
+
+              expect(
+                result['is_variable'],
+                isNull,
+              );
+
+              expect(
+                result['links'],
+                hasLength(1),
+              );
+
+              expect(
+                result['term'],
+                isNull,
+              );
+            },
+          );
+        },
+      );
+    },
+  );
+}
