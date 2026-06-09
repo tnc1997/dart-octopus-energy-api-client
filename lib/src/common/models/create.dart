919dd470944d350913563c9e986b14294f00e1af
Thomas Clark <tnc1997@outlook.com>
2026-06-09 07:45:00 +0100

    docs(common): add create model

--- lib/src/common/models/create.dart
+++ lib/src/common/models/create.dart
@@ -16,36 +16,137 @@ import '../../electricity_meter_points/models/electricity_meter_point.dart';
 import '../../gas_meter_points/models/gas_meter_point.dart';
 import 'address.dart';
 
+/// Serializer to create an account via a REST API.
+///
+/// Temporary to allow for legacy behaviour where uplifts do not count towards
+/// quoted amounts, which is potentially only OEGB behaviour.
 class Create {
+  /// A unique reference to previous external account number or similar account
+  /// identifier.
+  ///
+  /// The account reference must be in NAMESPACE-VALUE format.
   String? accountReference;
+
   AccountType accountType;
+
   Affiliate? affiliate;
+
   Address billingAddress;
+
+  /// Billing options for the account.
   BillingOptions? billingOptions;
+
+  /// A code to specify the account should be for a particular brand.
+  ///
+  /// This is only required when creating an account in an instance with
+  /// multiple brands and for which the requested brand is not the default brand
+  /// of that instance.
   BrandCode? brandCode;
+
   Business? business;
+
   Deposit? deposit;
+
+  /// At least one electricity or gas meter-point must be included. A maximum of
+  /// two electricity and one gas meter-point can be included. All meter-points
+  /// must be based in the Great Britain. All meter-points must belong to the
+  /// same address. For an electricity meter-point, at least one of 'mpan' or
+  /// 'address' must be included. At least one of 'consumption_standard'
+  /// (standard tariff) or a combination of 'consumption_day' and
+  /// 'consumption_night' (two-rate tariff) or 'consumption_day',
+  /// 'consumption_night' and 'consumption_off_peak' (three-rate tariff) must be
+  /// included depending on the type of tariff specified.
   List<ElectricityMeterPoint>? electricityMeterPoints;
+
+  /// At least one electricity or gas meter-point must be included. A maximum of
+  /// two electricity and one gas meter-point can be included. All meter-points
+  /// must be based in the Great Britain. All meter-points must belong to the
+  /// same address. For a gas meter-point, at least one of 'mprn' or 'address'
+  /// must be included, as well as 'consumption_standard'.
   List<GasMeterPoint>? gasMeterPoints;
+
+  /// Whether this account is a change of tenancy at the property.
   bool isChangeOfTenancy;
+
+  /// An array of key value pairs for storing generic metadata relating to an
+  /// account.
   List<Metadata>? metadata;
+
+  /// The payment details are required when using the method
+  /// 'MONTHLY_DIRECT_DEBIT' but are optional if all the meters are of type
+  /// PREPAYMENT'. The 'MONTHLY_DIRECT_PAYMENT' method is not available to all
+  /// users.
   Payment? payment;
+
+  /// A preferred supply start date.
+  ///
+  /// Only include this field if the earliest possible supply startdate is not
+  /// preferred.
   DateTime? preferredSsd;
+
   Address? previousBillingAddress;
+
+  /// A promotional code that will be applied to the account.
   String? promoRewardCode;
+
+  /// The code of an existing legacy quote created using the legacy 'CreatQuote'
+  /// mutation as part of the join supplier journey for this new customer. If
+  /// the quote was generated as part of an affiliate session, the affiliate
+  /// link and any query parameters belonging to the session will be associated
+  /// with the created account. Note: only one of 'quote_code' and
+  /// 'quote_request_code' should be provided in the input.
   String? quoteCode;
+
+  /// The code of an existing quote created using the 'QuoteNewMeterPoints'
+  /// mutation as part of the join supplier journey for this new customer. If
+  /// the quote was generated as part of an affiliate session, the affiliate
+  /// link and any query parameters belonging to the session will be associated
+  /// with the created account. Note: only one of 'quote_code' and
+  /// 'quote_request_code' should be provided in the input.
   String? quoteRequestCode;
+
+  /// A unique partner reference.
+  ///
+  /// The reference provided should be unique per switch.
   String reference;
+
+  /// The referral code of an existing account linked to a Kraken referral
+  /// scheme.
   String? referralCode;
+
+  /// Communication delivery preference
+  ///
+  /// When requires_postal_comms is True, then set to POSTAL, else set to EMAIL,
+  /// when not provided it will be null.
   bool? requiresPostalComms;
+
   SalesChannel? salesChannel;
+
+  /// An optional segmentation of the sales channel, e.g. the names of the
+  /// specific price comparison site, or the telesales organisation.
   String? salesSubchannel;
+
   String? signupRewardCode;
+
+  /// The occupier’s interest in installing a smart meter.
   SmartMeterInterest? smartMeterInterest;
+
+  /// The read permission frequency the occupier grants to meter readings from
+  /// smart meters.
   SmartMeterReadPermission? smartMeterReadPermission;
+
+  /// A code to indicate which type of smart tariff the customer is meant to
+  /// onboard onto.
+  ///
+  /// This is a restricted option. Please get in touch for information on
+  /// whether this can be enabled and what the allowed options are.
   SmartOnboardingProductType? smartOnboardingProductType;
+
+  /// A datetime of when the account sale was made.
   DateTime? soldAt;
+
   AccountSource source;
+
   List<User> users;
 
   Create({
