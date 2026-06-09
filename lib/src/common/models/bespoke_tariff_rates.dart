34e35cdca7f78c0336b23373a9c8ddd57269a71f
Thomas Clark <tnc1997@outlook.com>
2026-06-09 07:44:45 +0100

    docs(common): add bespoke tariff rates model

--- lib/src/common/models/bespoke_tariff_rates.dart
+++ lib/src/common/models/bespoke_tariff_rates.dart
@@ -1,8 +1,18 @@
 import 'bespoke_electricity_unit_rate.dart';
 
 class BespokeTariffRates {
+  /// The value in pence per day of the charge (excluding VAT).
   double? standingCharge;
+
+  /// The value in pence per kWh of the charge (excluding VAT).
+  ///
+  /// This field should be used for gas meters.
   double? unitRate;
+
+  /// List of the value in pence per kWh of the charges (excluding VAT).
+  ///
+  /// For elec meters, the unit rates are provided on a per register basis in
+  /// this array.
   List<BespokeElectricityUnitRate>? unitRates;
 
   BespokeTariffRates({
