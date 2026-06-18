f01a48f739d5c8b5958cbf529f4cac4534203a38
Thomas Clark <tnc1997@outlook.com>
2026-06-18 06:51:46 +0100

    refactor(common): update bespoke rate type enum

--- lib/src/v1/common/enums/bespoke_rate_type.dart
+++ lib/src/v1/common/enums/bespoke_rate_type.dart
@@ -3,12 +3,23 @@ enum BespokeRateType {
   eco7Day,
   eco7Night,
   evDevice,
+  evDeviceOffPeak,
+  evDevicePeak,
+  hmcGuaranteeAdvance,
+  hmcGuaranteeFull,
   nuclearRab,
   offPeak,
+  offPeakWeekends,
+  peak,
   standard,
+  summer,
   summerOffPeak,
   summerPeak,
-  tnous,
+  summerWeekendsOffPeakWinter,
+  tnuos,
+  weekday,
+  weekendsOther,
+  winter,
   winterOffPeak,
   winterPeak;
 
@@ -24,18 +35,40 @@ enum BespokeRateType {
         return BespokeRateType.eco7Night;
       case 'EV_DEVICE':
         return BespokeRateType.evDevice;
+      case 'EV_DEVICE_OFF_PEAK':
+        return BespokeRateType.evDeviceOffPeak;
+      case 'EV_DEVICE_PEAK':
+        return BespokeRateType.evDevicePeak;
+      case 'HMC_GUARANTEE_ADVANCE':
+        return BespokeRateType.hmcGuaranteeAdvance;
+      case 'HMC_GUARANTEE_FULL':
+        return BespokeRateType.hmcGuaranteeFull;
       case 'NUCLEAR_RAB':
         return BespokeRateType.nuclearRab;
       case 'OFF_PEAK':
         return BespokeRateType.offPeak;
+      case 'OFF_PEAK_WEEKENDS':
+        return BespokeRateType.offPeakWeekends;
+      case 'PEAK':
+        return BespokeRateType.peak;
       case 'STANDARD':
         return BespokeRateType.standard;
+      case 'SUMMER':
+        return BespokeRateType.summer;
       case 'SUMMER_OFF_PEAK':
         return BespokeRateType.summerOffPeak;
       case 'SUMMER_PEAK':
         return BespokeRateType.summerPeak;
+      case 'SUMMER_WEEKENDS_OFF_PEAK_WINTER':
+        return BespokeRateType.summerWeekendsOffPeakWinter;
       case 'TNUOS':
-        return BespokeRateType.tnous;
+        return BespokeRateType.tnuos;
+      case 'WEEKDAY':
+        return BespokeRateType.weekday;
+      case 'WEEKENDS_OTHER':
+        return BespokeRateType.weekendsOther;
+      case 'WINTER':
+        return BespokeRateType.winter;
       case 'WINTER_OFF_PEAK':
         return BespokeRateType.winterOffPeak;
       case 'WINTER_PEAK':
@@ -55,18 +88,40 @@ enum BespokeRateType {
         return 'ECO7_NIGHT';
       case BespokeRateType.evDevice:
         return 'EV_DEVICE';
+      case BespokeRateType.evDeviceOffPeak:
+        return 'EV_DEVICE_OFF_PEAK';
+      case BespokeRateType.evDevicePeak:
+        return 'EV_DEVICE_PEAK';
+      case BespokeRateType.hmcGuaranteeAdvance:
+        return 'HMC_GUARANTEE_ADVANCE';
+      case BespokeRateType.hmcGuaranteeFull:
+        return 'HMC_GUARANTEE_FULL';
       case BespokeRateType.nuclearRab:
         return 'NUCLEAR_RAB';
       case BespokeRateType.offPeak:
         return 'OFF_PEAK';
+      case BespokeRateType.offPeakWeekends:
+        return 'OFF_PEAK_WEEKENDS';
+      case BespokeRateType.peak:
+        return 'PEAK';
       case BespokeRateType.standard:
         return 'STANDARD';
+      case BespokeRateType.summer:
+        return 'SUMMER';
       case BespokeRateType.summerOffPeak:
         return 'SUMMER_OFF_PEAK';
       case BespokeRateType.summerPeak:
         return 'SUMMER_PEAK';
-      case BespokeRateType.tnous:
+      case BespokeRateType.summerWeekendsOffPeakWinter:
+        return 'SUMMER_WEEKENDS_OFF_PEAK_WINTER';
+      case BespokeRateType.tnuos:
         return 'TNUOS';
+      case BespokeRateType.weekday:
+        return 'WEEKDAY';
+      case BespokeRateType.weekendsOther:
+        return 'WEEKENDS_OTHER';
+      case BespokeRateType.winter:
+        return 'WINTER';
       case BespokeRateType.winterOffPeak:
         return 'WINTER_OFF_PEAK';
       case BespokeRateType.winterPeak:
